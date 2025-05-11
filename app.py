2c1a37461cb0cd0742ea696dca719b45086ee090
Shakthi <shakthinandanp0712@gmail.com>
2025-05-11 12:37:33 +0530

    add

--- app.py
+++ app.py
@@ -33,7 +33,9 @@ def get_location():
 	return jsonify(latest)
 @app.route('/')
 def homr():
-	return render_template("index4.html")
+	return render_template("index5.html")
 if __name__ == '__main__': 
-	app.run(host='0.0.0.0', port=5000)
+    app.run(ssl_context=('cert.pem', 'key.pem'),debug=True,host="0.0.0.0")
+
+	#app.run(host='0.0.0.0', port=5000)
 
